273f014a42a5887b247ea00219f16f7089a5ae68
danschreiberSIUE <daschre@siue.edu>
2025-10-24 10:27:05 -0500

    Update config with new Google Sheet URL

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/16m2Flm9KOqgLOQ81r6FwlssUUvuGB0wm8xvwqL0Z8qM/edit?usp=drivesdk';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
